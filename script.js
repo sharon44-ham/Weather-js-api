ebca62a45762e9a22888299dc3361416064f7917
Sharanya <maryalasharanya15@gmail.com>
2025-06-07 20:48:04 +0530

    updated

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const apiUrl="https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 let searchInput=document.querySelector(".search input")
 let searchBtn=document.querySelector(".search button")
 let weather =  document.querySelector(".weather-icon")
+
+
 async function checkWeather(city){
    const response= await fetch(apiUrl + city + `&appid=${apiKey}` )
+   if(response.status==404){
+    document.querySelector(".error").style.display="block"
+    document.querySelector(".weather").style.display="none"
+   }else{
     let data = await response.json()
-    console.log(data)
 
     document.querySelector(".city").innerHTML=data.name
     document.querySelector(".temp").innerHTML=data.main.temp+"°C"
@@ -23,6 +28,8 @@ async function checkWeather(city){
     }else if(data.weather[0].main=="Clear"){
        weather.src="images/clear.png"
     }
+    document.querySelector(".weather").style.display="block"
+   }
     
 }
 searchInput.addEventListener("keydown",function(Event){
@@ -33,5 +40,6 @@ searchInput.addEventListener("keydown",function(Event){
 searchBtn.addEventListener("click",function(){
     checkWeather(searchInput.value)
 })
+
 
 
